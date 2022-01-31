d62f5a3caa9016d2e518f4eeacad7ecb948a6317
talitaoc <talitaoc@gmail.com>
2022-01-31 11:24:03 -0300

    primeiro commit

---
+++ src/com/loiane/estruturadados/vetor/teste/Aula05.java
@@ -0,0 +1,20 @@
+package com.loiane.estruturadados.vetor.teste;
+
+import com.loiane.estruturadados.vetor.Vetor;
+
+public class Aula05 {
+
+	public static void main(String[] args) throws IllegalAccessException {
+		
+		Vetor vetor = new Vetor(10);
+		
+		vetor.adiciona("elemento 1");
+		vetor.adiciona("elemento 2");
+		vetor.adiciona("elemento 3");
+		
+		System.out.println(vetor.busca(2));
+		
+
+	}
+
+}
